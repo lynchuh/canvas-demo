d8478f7310b977d1b3059cdf44bfd6724025568b
lynchuh <lynchuh@gmail.com>
2018-04-16 19:51:25 +0800

    修改不能画画的bug

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ var context = canvas.getContext('2d');
 
  autoSetSize(canvas)
  clearScreen()
- listenToUser(canvas)
+ listenToMouse(canvas)
 
 
 /***函数***/
+
+/* 画圆圈
+function drawCircle(x,y,radius){
+  context.beginPath();
+  context.arc(x,y,radius,0,Math.PI*2);
+  context.stroke()
+  context.closePath()
+}
+*/
+
 function drawLine(x1,y1,x2,y2,){
   context.beginPath()
   context.moveTo(x1,y1)
@@ -27,15 +37,13 @@ function setCanvasSize(aaa){
   aaa.width = newWidth 
   aaa.height = newHeight
 }
-
-//设置canvas背景为白色
+//初始化屏幕背景
 function clearScreen(){
   context.fillStyle = '#fff'
   context.fillRect(0,0,canvas.width,canvas.height)
 }
 
-//使用状态设置
-function listenToUser(canvas){
+function listenToMouse(canvas){
   var using = false //创建Boolean变量控制使用（绘画/橡皮擦）状态，初始状态是false。
   var usingEraser = false//创建Boolean变量控制橡皮擦状态，初始状态是false。
   var lastPoint = {x:undefined,y:undefined} //创建全局变量，这样函数调用时在这个变量是存在的。
@@ -100,7 +108,7 @@ function listenToUser(canvas){
   }
   //工具使用
   clear.onclick = function(){
-    context.clearRect(0,0,canvas.width,canvas.height)
+    clearScreen()
   }
   save.onclick = function(){
     var url = canvas.toDataURL();
